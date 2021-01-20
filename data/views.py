9050c4bd96506a85c1cea507e7c53904e7aeb96b
오동석 <>
2021-01-20 16:22:01 +0900

    상단 항목 수정

--- data/views.py
+++ data/views.py
@@ -5,14 +5,11 @@ from django.shortcuts import render
 
 app_name = 'data'
 
-@login_required(login_url='common:login')
 def data1(request):
     return render(request, 'data/django_study.html', {})
 
-@login_required(login_url='common:login')
 def data2(request):
     return render(request, 'data/lotto.html', {})
 
-@login_required(login_url='common:login')
 def data3(request):
     return render(request, 'data/study02.html', {})
